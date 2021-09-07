614e3dd9f66c5d2d809a38e1784011cb66d9ce40
mauriciocoruja <mbjr@outlook.com.br>
2021-09-07 19:52:38 -0300

    Implemented Service layer, an endpoint to locate users by id

--- src/main/java/com/uniesquina/basicommerce/resources/UserResource.java
+++ src/main/java/com/uniesquina/basicommerce/resources/UserResource.java
@@ -1,20 +1,33 @@
 package com.uniesquina.basicommerce.resources;
 
 import com.uniesquina.basicommerce.entities.User;
+import com.uniesquina.basicommerce.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L,"Maria","maria@maria.com"
-                ,"999999999","12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 
 }
