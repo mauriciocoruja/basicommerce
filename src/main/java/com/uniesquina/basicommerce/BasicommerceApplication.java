9b1f62921d38d49fa62e4512b5e01b14a8f26e43
mauriciocoruja <mbjr@outlook.com.br>
2021-09-07 17:08:44 -0300

    First commit

---
+++ src/main/java/com/uniesquina/basicommerce/BasicommerceApplication.java
@@ -0,0 +1,13 @@
+package com.uniesquina.basicommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniesquina/basicommerce/BasicommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniesquina.basicommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
