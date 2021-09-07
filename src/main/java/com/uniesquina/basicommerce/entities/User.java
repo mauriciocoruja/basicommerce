288b5316f2d0cbf663142e52cda2ae590a1de62e
mauriciocoruja <mbjr@outlook.com.br>
2021-09-07 19:07:02 -0300

    implemented H2 as test database

--- src/main/java/com/uniesquina/basicommerce/entities/User.java
+++ src/main/java/com/uniesquina/basicommerce/entities/User.java
@@ -1,10 +1,17 @@
 package com.uniesquina.basicommerce.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class User implements Serializable {
     public static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
