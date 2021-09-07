d817c22602a1c7d65e9b37a33682667a04bb40f4
mauriciocoruja <mbjr@outlook.com.br>
2021-09-07 20:15:48 -0300

    Created entity Order and related to User by @OneToMany and @ManyToOne Hibernate anotation

--- src/main/java/com/uniesquina/basicommerce/entities/User.java
+++ src/main/java/com/uniesquina/basicommerce/entities/User.java
@@ -1,12 +1,12 @@
 package com.uniesquina.basicommerce.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     public static final long serialVersionUID = 1L;
 
@@ -17,6 +17,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -69,6 +72,10 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -83,4 +90,6 @@ public class User implements Serializable {
     public int hashCode() {
         return id != null ? id.hashCode() : 0;
     }
+
+
 }
