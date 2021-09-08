2ed724ad8682b4e129003411f892ceaa0e43dc77
mauriciocoruja <mbjr@outlook.com.br>
2021-09-08 00:13:42 -0300

    Implemented class enum to control order status

--- src/main/java/com/uniesquina/basicommerce/entities/Order.java
+++ src/main/java/com/uniesquina/basicommerce/entities/Order.java
@@ -1,6 +1,7 @@
 package com.uniesquina.basicommerce.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.uniesquina.basicommerce.enums.OrderStatus;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -18,7 +19,7 @@ public class Order implements Serializable {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
-//    private OrderStatus orderStatus;
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -28,9 +29,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -48,6 +50,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
