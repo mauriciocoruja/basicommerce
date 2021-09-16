e681f51d0849245a8d983cd6ab74dec0033d0eed
mauriciocoruja <mbjr@outlook.com.br>
2021-09-15 23:29:41 -0300

    Add method to insert, delete and update products

--- src/main/java/com/uniesquina/basicommerce/service/ProductService.java
+++ src/main/java/com/uniesquina/basicommerce/service/ProductService.java
@@ -1,10 +1,13 @@
 package com.uniesquina.basicommerce.service;
 
 import com.uniesquina.basicommerce.entities.Product;
+import com.uniesquina.basicommerce.entities.User;
 import com.uniesquina.basicommerce.repositories.ProductRepository;
+import com.uniesquina.basicommerce.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,6 +23,27 @@ public class ProductService {
 
     public Product findById(Long id) {
         Optional<Product> product = productRepository.findById(id);
-        return product.get();
+        return product.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Product insert(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product update(Long id, Product product) {
+        try {
+            Product entity = productRepository.getById(id);
+            updateData(entity, product);
+            return productRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Product entity, Product product) {
+        entity.setName(product.getName());
+        entity.setDescription(product.getDescription());
+        entity.setImgUrl(product.getImgUrl());
+        entity.setPrice(product.getPrice());
     }
 }
