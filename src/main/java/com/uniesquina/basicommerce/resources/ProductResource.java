0f08c73476f6ae5478bf15a1fdbe40dc64b8f154
mauriciocoruja <mbjr@outlook.com.br>
2021-09-16 23:12:27 -0300

    Add method to insert, delete and update products on resources

--- src/main/java/com/uniesquina/basicommerce/resources/ProductResource.java
+++ src/main/java/com/uniesquina/basicommerce/resources/ProductResource.java
@@ -4,11 +4,10 @@ import com.uniesquina.basicommerce.entities.Product;
 import com.uniesquina.basicommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -30,4 +29,26 @@ public class ProductResource {
         return ResponseEntity.ok().body(item);
     }
 
+    @PostMapping
+    public ResponseEntity<Product> insert(@RequestBody Product product) {
+        product = productService.insert(product);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("{/id}")
+                .buildAndExpand(product.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(product);
+    }
+
+    @DeleteMapping(value = "{/id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        productService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "{/id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product) {
+        product = productService.update(id, product);
+        return ResponseEntity.ok().body(product);
+    }
 }
--- src/main/java/com/uniesquina/basicommerce/service/ProductService.java
+++ src/main/java/com/uniesquina/basicommerce/service/ProductService.java
@@ -3,8 +3,11 @@ package com.uniesquina.basicommerce.service;
 import com.uniesquina.basicommerce.entities.Product;
 import com.uniesquina.basicommerce.entities.User;
 import com.uniesquina.basicommerce.repositories.ProductRepository;
+import com.uniesquina.basicommerce.service.exceptions.DatabaseException;
 import com.uniesquina.basicommerce.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
@@ -30,6 +33,16 @@ public class ProductService {
         return productRepository.save(product);
     }
 
+    public void delete(Long id) {
+        try {
+            productRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+    }
+
     public Product update(Long id, Product product) {
         try {
             Product entity = productRepository.getById(id);
@@ -43,7 +56,7 @@ public class ProductService {
     private void updateData(Product entity, Product product) {
         entity.setName(product.getName());
         entity.setDescription(product.getDescription());
-        entity.setImgUrl(product.getImgUrl());
         entity.setPrice(product.getPrice());
+        entity.setImgUrl(product.getImgUrl());
     }
 }
